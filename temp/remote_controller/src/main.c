8a5c32583aec1b96c15685abbeaa78fbfb76e373
aHaugl <andreas.haugland1@gmail.com>
2024-02-05 14:05:37 +0100

    test

---
+++ temp/remote_controller/src/main.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+#include <dk_buttons_and_leds.h>
+
+#define LOG_MODULE_NAME app
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+#define RUN_STATUS_LED DK_LED1
+#define RUN_LED_BLINK_INTERVAL 1000
+
+/* Callbacks */
+void button_handler(uint32_t button_state, uint32_t has_changed)
+{
+	int button_pressed = 0;
+	if (has_changed & button_state)
+	{
+		switch (has_changed)
+		{
+			case DK_BTN1_MSK:
+				button_pressed = 1;
+				break;
+			case DK_BTN2_MSK:
+				button_pressed = 2;
+				break;
+			case DK_BTN3_MSK:
+				button_pressed = 3;
+				break;
+			case DK_BTN4_MSK:
+				button_pressed = 4;
+				break;
+			default:
+				break;
+		}
+		LOG_INF("Button %d pressed.", button_pressed);
+	}
+}
+
+/* Configurations */
+static void configure_dk_buttons_and_leds(void){
+	int err;
+
+	err = dk_leds_init();
+	if(err){
+		LOG_ERR("Couldn't init LEDS (err %d)", err);
+	}
+	
+    err = dk_buttons_init(button_handler);
+    if (err) {
+        LOG_ERR("Couldn't init buttons (err %d)", err);
+    }
+}
+
+/* Main */
+int main(void)
+{
+	int blink_status = 0;
+	LOG_INF("Hello World! %s\n", CONFIG_BOARD);
+
+	configure_dk_buttons_and_leds();
+
+	for(;;){
+		dk_set_led(RUN_STATUS_LED,(blink_status++)%2);
+		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
+	}
+	return 0;
+}
